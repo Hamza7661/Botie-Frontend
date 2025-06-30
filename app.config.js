33219362b00436207a63a597fd2f8ac22783619a
Hamza <hamzaiftikhar@iaanswers.com>
2025-06-30 21:14:18 +0500

    bunder fixes

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ export default {
     },
     web: {
       favicon: "./assets/favicon.png",
-      bundler: "webpack"
+      bundler: "metro"
     },
     extra: {
       apiBaseUrl: "http://localhost:5000/api"
